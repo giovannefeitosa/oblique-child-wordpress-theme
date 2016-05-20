97b43113788a160e2e2d77b1018959d101203b28
Giovanne Afonso <giovanneafonso@gmail.com>
2016-05-20 01:52:39 -0400

    Fix problem with "time ago" function

--- contentheader.php
+++ contentheader.php
@@ -1,6 +1,6 @@
 <?php
 
-$contentheader_timeago = post_time_ago($post);
+$contentheader_timeago = human_time_diff(strtotime(explode(' ', $post->post_date)[0]));
 
 // <article class="hentry hentry-header" style="width:350px;height:259px;margin-bottom:30px;">
 ?>
