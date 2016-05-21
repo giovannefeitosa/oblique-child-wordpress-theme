875b645d1020bf3d440e23dd3aeb704d222cc839
Giovanne Afonso <giovanneafonso@gmail.com>
2016-05-21 14:54:57 -0400

    Update presentation header

--- home-header.php
+++ home-header.php
@@ -6,24 +6,35 @@
     <div class="taxonomy-description">É o que você deve estar se perguntando agora</div>
 </header><!-- .page-header -->
 <div class="page-header-content">
-    <div style="width:100%; max-width: 560px;float:right;margin-left:30px;">
+    <div class="page-header-video">
         <div class="video-container">
             <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/2EYeyBuj0sM?rel=0" frameborder="0" allowfullscreen></iframe>
         </div>
     </div>
 
     <p>
-        Quando você ouve a palavra SHAKE o que você pensa? Bom... eu penso em mim mesmo.
-        E aê, eu sou o SHAKE. Eu criei esse projeto para mostrar meu "antes" e "depois"
-        pra vocês. Agora eu ainda estou no "antes" asuhuas.
+        
+        Hey, eu sou o <strong>SHAKE</strong>! <img src="<?= get_stylesheet_directory_uri() ?>/images/shake-ghost.png" style="width:24px;">
+        Eu estou evoluindo todos os dias com o objetivo
+        de mudar a minha vida pra melhor (vou ficar rico caray).
+        E resolvi criar o <strong>Projeto SHAKE</strong> para mostrar como eu
+        <i>- um cara chato, com depressão, pobre, que bebe e fuma, preguiçoso, imaturo, que não sabe 
+        nem sequer cuidar de si mesmo e vive assistindo séries -</i>
+        vou me tornar um fucking empresário, youtuber, músico e que sabe se cuidar em qualquer
+        lugar.
+        
     </p>
     
-    <div class="text-right">
+    <p class="text-right">
         <span style="color:#eee;font-size:12px;">
             SAIBA MAIS AQUI
             <span style="color:#999;font-size:2em;">&raquo;</span>
         </span>
-    </div>
+    </p>
+    
+    <p>
+        Além disso, eu vou mudar o mundo, é só questão de tempo. Você duvida...? XD
+    </p>
     
     <p>
         
