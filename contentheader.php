8fb3c7083dc0d40809b460dbef09e67bb050e99a
Giovanne Afonso <giovanneafonso@gmail.com>
2016-05-23 12:50:44 -0400

    Minor post listing adjustments
    - Yellow background for posts from "diversos" category
    - Fix timeago gmt time

--- contentheader.php
+++ contentheader.php
@@ -1,6 +1,6 @@
 <?php
 
-$contentheader_timeago = human_time_diff(get_post_time( 'U' ));
+$contentheader_timeago = human_time_diff(get_post_time( 'U', true ));
 
 ?>
 <div class="content-timeago-header">
