cd262a821de03b7c57229f074575b4bb0952a0cf
Giovanne Afonso <giovanneafonso@gmail.com>
2016-06-09 16:15:24 -0400

    Make searched posts looks like normal posts

--- content-search.php
+++ content-search.php
@@ -1,27 +1,47 @@
-<?php
-/**
- * The template part for displaying results in search pages.
- *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- */
-?>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title(sprintf('<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h1>'); ?>
+	<div class="svg-container post-svg svg-block">
+		<?php echo oblique_svg_3(); ?>
+	</div>	
 
-		<?php if ('post' == get_post_type()) : ?>
-		<div class="entry-meta">
-			<?php oblique_child_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+	<?php if (has_post_thumbnail() && (get_theme_mod('index_feat_image') != 1)) : ?>
+		<div class="entry-thumb">
+			<?php the_post_thumbnail('oblique-entry-thumb'); ?>
+			<a class="thumb-link" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" ><i class="fa fa-link"></i></a>		
+		</div>	
+	<?php endif; ?>	
 
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+	<?php if (has_post_thumbnail() && (get_theme_mod('index_feat_image') != 1)) : ?>
+	<div class="post-inner">
+	<?php else : ?>
+	<div class="post-inner no-thumb">
+	<?php endif; ?>		
+		<header class="entry-header">
+			<?php the_title(sprintf('<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h1>'); ?>
 
-	<footer class="entry-footer">
-		<?php oblique_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+			<?php if ('post' == get_post_type() && !get_theme_mod('meta_index')) : ?>
+			<div class="entry-meta">
+				<?php oblique_child_posted_on(); ?>
+			</div><!-- .entry-meta -->
+			<?php endif; ?>
+		</header><!-- .entry-header -->
+
+		<div class="entry-content">
+			<?php the_excerpt(); ?>
+
+			<?php
+                wp_link_pages([
+                    'before' => '<div class="page-links">'.__('Pages:', 'oblique'),
+                    'after'  => '</div>',
+                ]);
+            ?>
+		</div><!-- .entry-content -->
+	</div>
+	<?php if (!get_theme_mod('read_more')) : ?>
+	<div class="read-more">
+		<a href="<?php the_permalink(); ?>"><?php echo __('Continue reading &hellip;', 'oblique'); ?></a>
+	</div>		
+	<?php endif; ?>
+	<div class="svg-container post-bottom-svg svg-block">
+		<?php echo oblique_svg_1(); ?>
+	</div>	
 </article><!-- #post-## -->
